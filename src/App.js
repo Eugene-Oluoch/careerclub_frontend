d1750e6a51b8e28c9ed745132f3b821d35cd8a52
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-11-12 10:09:42 +0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
+//States
+import { useState } from 'react'
+
+//Css
 import './App.css';
 
+
+//Components
+import Authentication from './components/authentication/Authentication';
+import Navbar from './components/mini-components/navbar/Navbar';
+import Newsletter from './components/newsletter/Newsletter';
+import Footer from './components/mini-components/footer/Footer';
+import Jobs from './components/jobs/Jobs';
+import Fof from './components/four-ow-four/Fof';
+import PostJob from './components/post-a-job/PostJob'
+import Companies from './components/companies/Companies';
+//Modules
+import { Routes,Route } from 'react-router-dom';
+
 function App() {
+
+  //States
+  let [close,setClose] = useState(false)
+
+  //States functions
+  let handleClose = ()=>{
+      close?setClose(false):setClose(true)
+  }
+
   return (
-    <section className="app"></section>
+    <>
+    <Navbar open={handleClose} />
+    <Routes>
+      <Route path="*" element={<Fof />} />
+      <Route path="/" element={<Jobs />} />
+      <Route path="/jobs" element={<Jobs />} />
+      <Route path="/companies" element={<Companies />} />
+      <Route path="/post-a-job" element={<PostJob />} />
+    </Routes>
+    <section className="app">
+      {
+        close?
+        <Authentication setClose={handleClose} close={close} />
+        :
+        ""
+      }
+      <Newsletter />
+      <Footer />
+    </section>
+    </>
   );
 }
 
